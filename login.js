37470dde10e20b0dd38c5109048ae7d98aa3b88c
MaxineK36 <maxine@algernon.com>
2017-03-17 10:52:23 -0400

    Add files via upload

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
- console.log("10:42 version working");
+ console.log("10:48 version working");
   $("#welcomeDiv").hide();
   $("#loginDiv").show();
 
@@ -47,7 +47,6 @@ var signIn = function(){
     
     
   }
-var user = firebase.auth().currentUser;
 
 firebase.auth().onAuthStateChanged(function(user) {
   if ((user)&&(test1===true)) {
@@ -68,18 +67,29 @@ if($("#navbarRow").length > 0){
 }
 
   var welcomeUser = function(){
-    user = firebase.auth().currentUser;
-    var name, email, photoUrl, uid, emailVerified;
-    console.log("starting to welcome user");
+    var user = firebase.auth().currentUser;
+
     if (user != null) {
-      name = firebase.auth().currentUser.displayName;
-      email = user.email;
-      photoUrl = user.photoURL;
-      emailVerified = user.emailVerified;
-      uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-                       // this value to authenticate with your backend server, if
-                      // you have one. Use User.getToken() instead.
-    }
+      user.providerData.forEach(function (profile) {
+        console.log("Sign-in provider: "+profile.providerId);
+        console.log("  Provider-specific UID: "+profile.uid);
+        console.log("  Name: "+profile.displayName);
+        console.log("  Email: "+profile.email);
+        console.log("  Photo URL: "+profile.photoURL);
+      });
+    }    
+
+  var name, email, photoUrl, uid, emailVerified;
+    console.log("starting to welcome user");
+    // if (user != null) {
+    //   name = firebase.auth().currentUser.displayName;
+    //   email = user.email;
+    //   photoUrl = user.photoURL;
+    //   emailVerified = user.emailVerified;
+    //   uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+    //                    // this value to authenticate with your backend server, if
+    //                   // you have one. Use User.getToken() instead.
+    // }
     console.log(name);
       console.log("opening modal")
       document.getElementById("welcomeBar").innerHTML = "Welcome, " + name;
